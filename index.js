cc61b7f96ec44c34ee9fe1c7bdc7d3b60b938f09
thovagaushop <minhmeomeomun@gmail.com>
2022-05-18 10:38:59 +0700

    Upload server

--- index.js
+++ index.js
@@ -1,68 +1,65 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
 import dotenv from 'dotenv';
-import fetch, { Headers } from 'node-fetch';
-import cors from 'cors';
 import { Shopify } from '@shopify/shopify-api';
-
+import cors from 'cors';
 dotenv.config();
 
+const host = '127.0.0.1';
 const port = 3000;
 
-const { SHOPIFY_API_KEY, SHOPIFY_API_SECRET_KEY, SHOPIFY_API_SCOPES, HOST } = process.env;
 const shops = {};
 
+const {SHOPIFY_API_KEY, SHOPIFY_API_SECRET, SHOPIFY_API_SCOPES, HOST} = process.env;
+
 Shopify.Context.initialize({
-    API_KEY : SHOPIFY_API_KEY,
-    API_SECRET_KEY : SHOPIFY_API_SECRET_KEY,
+    API_KEY: SHOPIFY_API_KEY,
+    API_SECRET_KEY: SHOPIFY_API_SECRET,
     SCOPES: SHOPIFY_API_SCOPES,
-    HOST_NAME: HOST,
-    IS_EMBEDDED_APP: false,
+    HOST_NAME: HOST.replace(/https?:\/\//, ""),
+    HOST_SCHEME: HOST.split("://")[0],
+    IS_EMBEDDED_APP: true,
 });
 
 const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
 app.use(cors());
 
-app.get('/', async (req, res, next) => {
+app.get('/', async(req, res) => {
     if (typeof shops[req.query.shop] !== 'undefined') {
-        console.log(req.session.products)
-        res.json(req.session.products);
+        res
     } else {
         res.redirect(`/login?shop=${req.query.shop}`);
     }
-})
+});
 
-app.get(`/login`, async (req, res, next) => {
-    let authRoute = await Shopify.Auth.beginAuth(
+app.get('/login', async(req, res) => {
+    const authRoute = await Shopify.Auth.beginAuth(
         req,
         res,
         req.query.shop,
         '/auth/callback',
         false,
-    );
-
-    return res.redirect(authRoute);
+    )
+    res.redirect(authRoute);
 })
 
-app.get('/auth/callback', async (req, res, next) => {
+app.get('/auth/callback', async(req, res) => {
     const session = await Shopify.Auth.validateAuthCallback(
         req,
         res,
         req.query
-    );
-    shops[session] = session;
-    console.log(shops[session]);
+      );
 
-    res.redirect('/');
-})
+    const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
+
+    const products = await client.get({
+        path : 'products'
+    });
+
+    console.log(products.json());
+});
+
 
 app.listen(port, () => {
-    console.log(`Server is running on port :  ${port}`)
-})
+    console.log(`Server running at http://${host}:${port}/`);
+});
