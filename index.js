e9788d4d879bc188bca94ebb239a3f52fbee6545
thovagaushop <minhmeomeomun@gmail.com>
2022-05-19 13:29:20 +0700

    Tesst 19/5/2022

--- index.js
+++ index.js
@@ -1,65 +1,57 @@
 import express from "express";
 import dotenv from 'dotenv';
-import { Shopify } from '@shopify/shopify-api';
-import cors from 'cors';
+import {Shopify} from '@shopify/shopify-api';
 dotenv.config();
 
-const host = '127.0.0.1';
-const port = 3000;
-
-const shops = {};
-
-const {SHOPIFY_API_KEY, SHOPIFY_API_SECRET, SHOPIFY_API_SCOPES, HOST} = process.env;
+const {SHOP, SHOPIFY_API_KEY, SHOPIFY_API_SECRET, SHOPIFY_API_SCOPES, HOST} = process.env;
 
 Shopify.Context.initialize({
     API_KEY: SHOPIFY_API_KEY,
     API_SECRET_KEY: SHOPIFY_API_SECRET,
     SCOPES: SHOPIFY_API_SCOPES,
-    HOST_NAME: HOST.replace(/https?:\/\//, ""),
-    HOST_SCHEME: HOST.split("://")[0],
-    IS_EMBEDDED_APP: true,
+    HOST_NAME : HOST,
+    IS_EMBEDDED_APP : true,
 });
+
+const shops = {};
 
 const app = express();
 
-app.use(cors());
+app.get("/", async (req, res) => {
+   if (typeof shops[req.query.shop] === 'undefined') {
+     res.redirect(`/login?shop=${req.query.shop}`);
+   } else {
+     res.send(req.query.accessToken);
+   }
+ });
 
-app.get('/', async(req, res) => {
-    if (typeof shops[req.query.shop] !== 'undefined') {
-        res
-    } else {
-        res.redirect(`/login?shop=${req.query.shop}`);
-    }
+ app.get('/login', async (req, res) => {
+    let authRoute = await Shopify.Auth.beginAuth(
+      req,
+      res,
+      req.query.shop,
+      '/auth/callback',
+      true,
+    );
+    console.log(authRoute);
+    res.redirect(authRoute);
 });
 
-app.get('/login', async(req, res) => {
-    const authRoute = await Shopify.Auth.beginAuth(
-        req,
-        res,
-        req.query.shop,
-        '/auth/callback',
-        false,
-    )
-    res.redirect(authRoute);
-})
-
-app.get('/auth/callback', async(req, res) => {
-    const session = await Shopify.Auth.validateAuthCallback(
+app.get('/auth/callback', async (req, res) => {
+    try {
+      const session = await Shopify.Auth.validateAuthCallback(
         req,
         res,
         req.query
       );
-
-    const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
-
-    const products = await client.get({
-        path : 'products'
-    });
-
-    console.log(products.json());
+      shops[session] = session.scope;
+      console.log(session.accessToken);
+    } catch (error) {
+      console.error(error);
+    }
+    res.redirect(`/?accessToken=${session.accessToken}`);
 });
 
-
-app.listen(port, () => {
-    console.log(`Server running at http://${host}:${port}/`);
+app.listen(3000, () => {
+console.log('your app is now listening on port 3000');
 });
