edbc5c1d5d86eb8a4ae32f203e0e0762557795f3
thovagaushop <minhmeomeomun@gmail.com>
2022-05-20 10:24:26 +0700

    Xong backend 20/5/2022

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from 'dotenv';
 import {Shopify} from '@shopify/shopify-api';
+import cors from 'cors';
+import fetch from 'node-fetch';
 dotenv.config();
 
 const {SHOP, SHOPIFY_API_KEY, SHOPIFY_API_SECRET, SHOPIFY_API_SCOPES, HOST} = process.env;
@@ -17,11 +19,13 @@ const shops = {};
 
 const app = express();
 
+app.use(cors());
+
 app.get("/", async (req, res) => {
    if (typeof shops[req.query.shop] === 'undefined') {
      res.redirect(`/login?shop=${req.query.shop}`);
    } else {
-     res.send(req.query.accessToken);
+        res.redirect(`/products?shop=${req.query.shop}`);
    }
  });
 
@@ -31,26 +35,42 @@ app.get("/", async (req, res) => {
       res,
       req.query.shop,
       '/auth/callback',
-      true,
+      false,
     );
-    console.log(authRoute);
     res.redirect(authRoute);
 });
 
 app.get('/auth/callback', async (req, res) => {
-    try {
       const session = await Shopify.Auth.validateAuthCallback(
         req,
         res,
         req.query
       );
-      shops[session] = session.scope;
-      console.log(session.accessToken);
-    } catch (error) {
-      console.error(error);
-    }
-    res.redirect(`/?accessToken=${session.accessToken}`);
+      console.log(session);
+      shops[session.shop] = session;
+      res.redirect(`/products?shop=${req.query.shop}`);
 });
+
+app.get('/products', async(req, res) => {
+    if (shops[req.query.shop] !== undefined) {
+        var result = await fetch(`https://${req.query.shop}/admin/api/2022-04/products.json`, {
+            method : 'get',
+            headers : {
+                'X-Shopify-Access-Token' : shops[req.query.shop].accessToken,
+            }
+        });
+        result = await result.json();
+        const products = result.products;
+        console.log(products);
+        res.json(products);
+    } else {
+        res.redirect(`/login?shop=${req.query.shop}`);
+    }
+    
+    // products = result.products.products;
+    // console.log(products);
+    // res.json(products);
+})
 
 app.listen(3000, () => {
 console.log('your app is now listening on port 3000');
